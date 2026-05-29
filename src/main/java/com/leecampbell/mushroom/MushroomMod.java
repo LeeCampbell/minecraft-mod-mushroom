48513039674738a2a67a525e0c05e0638cbce7d6
Lee Campbell <lee.ryan.campbell@gmail.com>
2026-05-29 15:37:16 +0000

    Task 6.2: Add chat message on power removal (hit)

--- src/main/java/com/leecampbell/mushroom/MushroomMod.java
+++ src/main/java/com/leecampbell/mushroom/MushroomMod.java
@@ -2,6 +2,7 @@ package com.leecampbell.mushroom;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +19,7 @@ public class MushroomMod implements ModInitializer {
         ServerLivingEntityEvents.AFTER_DAMAGE.register((entity, source, baseDamageTaken, damageTaken, blocked) -> {
             if (entity instanceof ServerPlayer serverPlayer && baseDamageTaken > 0 && MushroomPowerManager.hasPower(serverPlayer)) {
                 MushroomPowerManager.removePower(serverPlayer);
+                serverPlayer.sendSystemMessage(Component.literal("Your mushroom power fades!"));
             }
         });
 
